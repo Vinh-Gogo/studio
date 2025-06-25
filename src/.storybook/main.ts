69c24da232b70e008bcd5ce5caa13118e58a3766
LeQuangVinh <Lea26462@gmail.com>
2025-06-25 05:58:29 +0000

    fix lỗi sau khi cập nhật sotrybook

--- src/.storybook/main.ts
+++ src/.storybook/main.ts
@@ -1,20 +1,19 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+    "@storybook/addon-docs"
   ],
+
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
-  staticDirs: ['../public'],
+
+  staticDirs: ['../../public']
 };
 export default config;
