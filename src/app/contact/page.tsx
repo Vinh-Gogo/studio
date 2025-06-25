e062d32938c67385c330c86e8b31d6956eef8157
LeQuangVinh <Lea26462@gmail.com>
2025-06-25 09:11:07 +0000

    Chỉ bản đồ  đến 12 Nguyễn Văn Bảo, Phường 1, Gò Vấp, Hồ Chí Minh 700000

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -28,14 +28,14 @@ export default function ContactPage() {
                     <CardContent>
                         <div className="aspect-video w-full rounded-md overflow-hidden border">
                             <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.4770094770837!2d106.6853969745973!3d10.851219857827003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529b6a2b351a5%3A0x11690ada8c36f9bc!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2hp4buHcCBUUC5IQ0M!5e0!3m2!1svi!2s!4v1722006764516!5m2!1svi!2s"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.4770094770837!2d106.68539697593259!3d10.851219857827003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529b6a2b351a5%3A0x11690ada8c36f9bc!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2hp4buHcCBUUC5IQ0M!5e0!3m2!1svi!2s!4v1722421319771!5m2!1svi!2s"
                                 width="100%"
                                 height="100%"
                                 style={{ border: 0 }}
                                 allowFullScreen={true}
                                 loading="lazy"
                                 referrerPolicy="no-referrer-when-downgrade"
-                                title="Bản đồ vị trí Trường Đại học Công nghiệp TP.HCM"
+                                title="Bản đồ vị trí 12 Nguyễn Văn Bảo, Phường 1, Gò Vấp, Hồ Chí Minh"
                             ></iframe>
                         </div>
                     </CardContent>
